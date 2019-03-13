ce397975ba7b13e6166c1992d1da5334dce61933
jam357 <jam357@uclive.ac.nz>
2019-03-13 14:14:51 +1300

    Added image, changed button layouts and created two new activities: HitchARideActivity and GiveARideActivity

--- app/src/main/java/com/example/project1/MainActivity.kt
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.project1
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun hitchARide(view: View){
+        val intent = Intent(this, HitchARideActivity::class.java).apply {
+        }
+        startActivity(intent)
+    }
+
+    fun giveARide(view: View){
+        val intent = Intent(this, GiveARideActivity::class.java).apply {
+        }
+        startActivity(intent)
     }
 }
