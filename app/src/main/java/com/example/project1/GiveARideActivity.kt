f77271c8438b7631be8031727354681e13724f18
jam357 <jam357@uclive.ac.nz>
2019-03-20 23:08:45 +1300

    Created form for Give a ride and started list for hitch a ride

--- app/src/main/java/com/example/project1/GiveARideActivity.kt
+++ app/src/main/java/com/example/project1/GiveARideActivity.kt
@@ -3,6 +3,7 @@ package com.example.project1
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+
 class GiveARideActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/project1/HitchARideActivity.kt
+++ app/src/main/java/com/example/project1/HitchARideActivity.kt
@@ -1,12 +1,41 @@
 package com.example.project1
 
-import android.support.v7.app.AppCompatActivity
+import android.app.AlertDialog
+import android.app.ListActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
-class HitchARideActivity : AppCompatActivity() {
+class HitchARideActivity : ListActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_hitch_aride)
+        listAdapter = ArrayAdapter<Friend>(this, android.R.layout.simple_list_item_1, friends)
+    }
+
+    private val friends = arrayOf<Friend>(
+        Friend("Joshua Meneghini", "joshuameneghini@gmail.com", "0273483623", "12 Moorpark Place")
+    )
+
+    override fun onListItemClick(l: ListView?, v: View?, friendId: Int, id: Long) {
+        val options = arrayOf("Map", "Email", "Text", "Call")
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Connect how?")
+        builder.setItems(options) { _, optionId ->
+            dispatchAction(optionId, friends[friendId])
+        }
+        builder.show()
+    }
+
+    fun dispatchAction(optionId: Int, friend: Friend) {
+        when (optionId) {
+            0 -> TODO("Map")
+            1 -> TODO("Email")
+            2 -> TODO("Text")
+            3 -> TODO("Call")
+        }
     }
 }
+
