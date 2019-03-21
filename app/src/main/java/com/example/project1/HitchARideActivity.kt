64589c4855ab512b04385a5ab931ad2e0aa0e14f
jam357 <jam357@uclive.ac.nz>
2019-03-22 12:05:03 +1300

    Created a list view of each 'ride' with custom rows

--- app/src/main/java/com/example/project1/HitchARideActivity.kt
+++ app/src/main/java/com/example/project1/HitchARideActivity.kt
@@ -3,33 +3,50 @@ package com.example.project1
 import android.app.AlertDialog
 import android.app.ListActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import java.time.LocalDate
+import java.util.*
 
-class HitchARideActivity : ListActivity() {
+class HitchARideActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        listAdapter = ArrayAdapter<Friend>(this, android.R.layout.simple_list_item_1, friends)
+        setContentView(R.layout.activity_hitch_aride)
+
+        var listView = findViewById<ListView>(R.id.rides_list)
+        // 1
+        val rideList = rides
+
+        val arrayAdapter = RideAdapter(this, rideList)
+
+        listView.adapter = arrayAdapter
+
+//        listView.setOnItemClickListener { _, _, position, _ ->
+//            val selectedRide = rideList[position]
+//
+//            val options = arrayOf("Map", "Email", "Text", "Call")
+//            val builder = AlertDialog.Builder(this)
+//            builder.setTitle("Connect how?")
+//            builder.setItems(options) { _, optionId ->
+//                dispatchAction(optionId, selectedRide)
+//            }
+//            builder.show()
+//        }
+
     }
 
-    private val friends = arrayOf<Friend>(
-        Friend("Joshua Meneghini", "joshuameneghini@gmail.com", "0273483623", "12 Moorpark Place")
+    private val rides = listOf<Ride>(
+        Ride("Joshua Meneghini", "joshuameneghini@gmail.com", "0273483623", "Mt Hutt", "2", "30-03-2019", "08:00", "Going early"),
+        Ride("Jed O'Brien", "jedobrien@gmail.com", "0256987631", "Mt Ruapehu", "3", "30-03-2019", "07:00", "Going early"),
+        Ride("Jess Eagan", "jesseagan@gmail.com", "0226987451", "Mt Coronet Peak", "2", "30-03-2019", "06:40", "Going early"),
+        Ride("Adam Rundle", "adamrundle@gmail.com", "0215896434", "Mt Remarkables", "1", "30-03-2019", "07:25", "Going early")
     )
 
-    override fun onListItemClick(l: ListView?, v: View?, friendId: Int, id: Long) {
-        val options = arrayOf("Map", "Email", "Text", "Call")
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("Connect how?")
-        builder.setItems(options) { _, optionId ->
-            dispatchAction(optionId, friends[friendId])
-        }
-        builder.show()
-    }
-
-    fun dispatchAction(optionId: Int, friend: Friend) {
+    fun dispatchAction(optionId: Int, ride: Ride) {
         when (optionId) {
             0 -> TODO("Map")
             1 -> TODO("Email")
---
+++ app/src/main/java/com/example/project1/Ride.kt
@@ -0,0 +1,5 @@
+package com.example.project1
+
+class Ride(val name: String, val email: String, val cellphone: String, val mountain: String, val availableSeats: String, val date: String, val time: String, val comments: String) {
+
+}
