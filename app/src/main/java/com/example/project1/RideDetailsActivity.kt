f430fcbcf941dee5e35b6bf5a05b50c6132e37ca
jam357 <jam357@uclive.ac.nz>
2019-03-25 14:45:19 +1300

    Implemented ability to invoke other apps on the system via an implicit intent

--- app/src/main/java/com/example/project1/RideDetailsActivity.kt
+++ app/src/main/java/com/example/project1/RideDetailsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.project1
 
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
@@ -44,10 +45,48 @@ class RideDetailsActivity : AppCompatActivity() {
 
 
     fun dispatchAction(optionId: Int, ride: Ride?) {
+        val name = ride?.name
+        val mountain = ride?.mountain
+        val availableSeats = ride?.availableSeats
+        val cellphone = ride?.cellphone
+        val emailArray = arrayOf(ride?.email)
+        val subject = "Ride share via SnowHitched"
+        val text = "Hey, $name! I'd like to take your $availableSeats seats to $mountain, cheers!"
         when (optionId) {
-            0 -> TODO("Email")
-            1 -> TODO("Text")
-            2 -> TODO("Call")
+            0 -> composeEmail(emailArray, subject, text)
+            1 -> composeMmsMessage(text, cellphone)
+            2 -> dialPhoneNumber(cellphone)
+        }
+    }
+
+    fun composeEmail(addresses: Array<String?>, subject: String, text: String) {
+        val intent = Intent(Intent.ACTION_SEND).apply {
+            type = "text/plain"
+            putExtra(Intent.EXTRA_EMAIL, addresses)
+            putExtra(Intent.EXTRA_TEXT, text)
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+
+    fun dialPhoneNumber(phoneNumber: String?) {
+        val intent = Intent(Intent.ACTION_DIAL).apply {
+            data = Uri.parse("tel:$phoneNumber")
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+
+    fun composeMmsMessage(message: String, cellphone: String?) {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("smsto:$cellphone")  // This ensures only SMS apps respond
+            putExtra("sms_body", message)
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
         }
     }
 }
